539319421c8ce0f61fbb1a9870d2c7a51a487480
Omar Shaikh <omarasifshaikh@hotmail.com>
2015-07-30 21:00:00 -0700

    Hello World MVC setup using Thymleaf

--- src/main/java/com/arvos/server/controller/AugmentController.java
+++ src/main/java/com/arvos/server/controller/AugmentController.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -53,4 +54,14 @@ public class AugmentController {
     	return Lists.newArrayList(augmentRepository_.findAll());
 		
 	}
+    
+    
+    
+    //START OF MVC
+    
+    @RequestMapping(value="/greeting", method=RequestMethod.GET)
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
 }
